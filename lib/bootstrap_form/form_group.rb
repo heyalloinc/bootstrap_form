0cf89d4e1d39d6d5355e4909d8fd87a3eafbd473
Istvan Pusztai <istvanp@gmail.com>
2020-10-03 01:49:51 -0400

    Adding support for floating labels

--- lib/bootstrap_form/form_group.rb
+++ lib/bootstrap_form/form_group.rb
@@ -35,6 +35,10 @@ module BootstrapForm
     def form_group_content(label, help_text, options, &block)
       if group_layout_horizontal?(options[:layout])
         concat(label).concat(tag.div(capture(&block) + help_text, class: form_group_control_class(options)))
+      elsif options[:layout] == :floating
+        concat(capture(&block))
+        concat(label)
+        concat(help_text) if help_text
       else
         concat(label)
         concat(capture(&block))
@@ -51,6 +55,7 @@ module BootstrapForm
 
     def form_group_classes(options)
       classes = ["mb-3", options[:class].try(:split)].flatten.compact
+      classes << "form-label-group" if options[:layout] == :floating
       classes << "row" if horizontal_group_with_gutters?(options[:layout], classes)
       classes << "col-auto g-3" if field_inline_override?(options[:layout])
       classes << feedback_class if options[:icon]
